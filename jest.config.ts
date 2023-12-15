724cf80b635d927f298dc0d6884fe5a215b29f7c
Alex Eagle <theherocraft@gmail.com>
2023-12-15 16:36:25 +0100

    Jest

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { recursive } = require('merge')
+const mongo = require('@shelf/jest-mongodb/jest-preset')
+
+const config = recursive(mongo, {
+  roots: [
+    '<rootDir>/src/',
+    '<rootDir>/tests/'
+  ],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/interfaces/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testMatch: [
+    '<rootDir>/tests/**/*.test.ts'
+  ],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
+  },
+  testPathIgnorePatterns: [
+    'node_modules'
+  ],
+  watchPathIgnorePatterns: [
+    'globalConfig'
+  ]
+})
+
+module.exports = config
